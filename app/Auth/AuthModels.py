746ef32a80fb09ef4e22aeefd674a3a078af21f6
Sayyed Mohammad Razavi <smrazavi1991@hotmail.com>
2023-03-13 10:24:35 +0330

    Basic design of auth pages ((front and back)) and add image for contact us page

--- app/Auth/AuthModels.py
+++ app/Auth/AuthModels.py
@@ -1,17 +1,20 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from pathlib import Path
 
 
-templates = Jinja2Templates(directory="Template")
+BASE_PATH = Path(__file__).resolve().parent.parent
+templates = Jinja2Templates(directory=str(BASE_PATH / "Templates"))
 router = APIRouter(prefix="/auth")
 
 
-# @router.get("/login", response_class=HTMLResponse)
-# pass
-#
-#
-# @router.get("/register", response_class=HTMLResponse)
-# pass
-#
-#
+@router.get("/login", response_class=HTMLResponse)
+async def login(request: Request):
+    return templates.TemplateResponse("Login.html", {"request": request})
+
+
+@router.get("/register", response_class=HTMLResponse)
+async def register(request: Request):
+    return templates.TemplateResponse("Register.html", {"request": request})
+

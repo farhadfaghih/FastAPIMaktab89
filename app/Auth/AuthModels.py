ad974d41fe59985698f66a99feddb2690cb0517a
Sayyed Mohammad Razavi <smrazavi1991@hotmail.com>
2023-03-13 12:19:56 +0330

    Complete login endpoint

--- app/Auth/AuthModels.py
+++ app/Auth/AuthModels.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends, HTTPException
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from pathlib import Path
+from .Schema import LoginRequest
+from..Core import models, dependencies
 
 
 BASE_PATH = Path(__file__).resolve().parent.parent
@@ -12,6 +14,15 @@ router = APIRouter()
 @router.get("/login", response_class=HTMLResponse)
 async def login(request: Request):
     return templates.TemplateResponse("Login.html", {"request": request})
+
+
+@router.post("/login")
+async def manage_user_login_request(login_request: LoginRequest, db=Depends(dependencies.get_db)):
+    user_check = db.query(models.User).filter(models.User.username == login_request.username).first()
+    if user_check and user_check.password == login_request.password:
+        return True
+    else:
+        raise HTTPException(status_code=404)
 
 
 @router.get("/register", response_class=HTMLResponse)

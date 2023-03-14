bf6b97c3d6062ec2246a750599d19cc0528cb736
Sayyed Mohammad Razavi <smrazavi1991@hotmail.com>
2023-03-14 10:55:22 +0330

    Add some new pages for: single post, all posts and admin and superuser dashboard. then create their endpoints. and finally do initial work to connect each endpoint to its front

--- app/Admin/AdminModels.py
+++ app/Admin/AdminModels.py
@@ -1,14 +1,14 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from pathlib import Path
 
 
-templates = Jinja2Templates(directory="Template")
+BASE_PATH = Path(__file__).resolve().parent.parent
+templates = Jinja2Templates(directory=str(BASE_PATH / "Templates"))
 router = APIRouter(prefix="/admin")
 
 
-# @router.get("/dashboard", response_class=HTMLResponse)
-# pass
-
-
-
+@router.get("/dashboard", response_class=HTMLResponse)
+async def admin_dashboard(request: Request):
+    return templates.TemplateResponse("Admin_dashboard.html", {"request": request})
--- app/Superuser/SuperuserModels.py
+++ app/Superuser/SuperuserModels.py
@@ -1,15 +1,17 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from pathlib import Path
 
 
-templates = Jinja2Templates(directory="Template")
+BASE_PATH = Path(__file__).resolve().parent.parent
+templates = Jinja2Templates(directory=str(BASE_PATH / "Templates"))
 router = APIRouter(prefix="/superuser")
 
 
-# @router.get("/dashboard", response_class=HTMLResponse)
-# pass
-#
+@router.get("/dashboard", response_class=HTMLResponse)
+async def admin_dashboard(request: Request):
+    return templates.TemplateResponse("Superuser_dashboard.html", {"request": request})
 #
 # @router.get("/posts", response_class=HTMLResponse)
 # pass

b77ad72258b8de10cfef393da7f3eacdd508e305
Sayyed Mohammad Razavi <smrazavi1991@hotmail.com>
2023-03-14 13:57:22 +0330

    Complete Posts page and make all home and Posts page posts show their writer

--- app/Home/HomeModels.py
+++ app/Home/HomeModels.py
@@ -14,7 +14,9 @@ router = APIRouter()
 
 @router.get("/", response_class=HTMLResponse)
 async def root(request: Request, db=Depends(get_db)):
-    recent_posts = db.query(Post).order_by(Post.id).limit(5).all()
+    # recent_posts = db.query(Post).order_by(Post.id).limit(5).all()
+    recent_posts = db.query(Post.id, Post.image, Post.title, Post.body, Post.create_date, Post.owner_id,
+                            User.fullname).join(User, Post.owner_id == User.id).order_by(Post.id).limit(3).all()
     return templates.TemplateResponse("Home.html", {"request": request, "posts": recent_posts})
 
 
@@ -28,11 +30,11 @@ async def sendmessage(messages: Messages, db=Depends(get_db)) -> None:
     """
     Create a message for Superuser
     :param db: call database to store user messages
-    :param message: message of user that comes from front-end. Must check with pydantic model to validate.
+    :param messages: message of user that comes from front-end. Must check with pydantic model to validate.
     :return: None
     """
-    db_message = message(created_by=messages.created_by, date_created=datetime.now(), email=messages.email,
-                                description=messages.description)
+    db_message = message(created_by=messages.created_by, date_created=datetime.now().strftime("%b %d, %Y"),
+                         email=messages.email, description=messages.description)
     db.add(db_message)
     db.commit()
     db.refresh(db_message)
--- app/Posts/PostsModels.py
+++ app/Posts/PostsModels.py
@@ -4,11 +4,11 @@ from fastapi.responses import HTMLResponse
 from app.Core.models import *
 from app.Core.dependencies import *
 from pathlib import Path
-
+from sqlalchemy import join
 
 BASE_PATH = Path(__file__).resolve().parent.parent
 templates = Jinja2Templates(directory=str(BASE_PATH / "Templates"))
-router = APIRouter(prefix="/posts")
+router = APIRouter(prefix="/post")
 
 
 @router.get("/{post_id}", response_class=HTMLResponse)
@@ -17,7 +17,8 @@ async def detail_post(id, request: Request, db=Depends(get_db)):
     return templates.TemplateResponse("Post_with_detail.html", {"request": request, "post": post})
 
 
-@router.get("/allposts", response_class=HTMLResponse)
+@router.get("/allposts/", response_class=HTMLResponse)
 async def all_posts(request: Request, db=Depends(get_db)):
-    posts = db.query(Post).all()
-    return templates.TemplateResponse("Posts.html", {"request": request, "posts": posts})
+    allposts = db.query(Post.id, Post.image, Post.title, Post.body, Post.create_date, Post.owner_id,
+                        User.fullname).join(User, Post.owner_id == User.id).all()
+    return templates.TemplateResponse("Posts.html", {"request": request, "posts": allposts})

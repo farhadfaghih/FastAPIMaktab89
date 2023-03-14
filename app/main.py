a4b9cc7d08422be77ac5fea16b0c22bb18b07298
Sayyed Mohammad Razavi <smrazavi1991@hotmail.com>
2023-03-14 20:03:26 +0330

    add auth for admin page

--- app/main.py
+++ app/main.py
@@ -13,8 +13,8 @@ from Posts import PostsModels
 from Superuser import SuperuserModels
 
 app = FastAPI(dependencies=[Depends(dependencies.get_db)])
-script_dir = os.path.dirname(__file__)[0:-4]
-st_abs_file_path = os.path.join(script_dir, "app\static")
+script_dir = os.path.dirname(__file__)
+st_abs_file_path = os.path.join(script_dir, "static")
 app.mount("/static", StaticFiles(directory=st_abs_file_path), name="static")
 database.Base.metadata.create_all(bind=database.engine)
 

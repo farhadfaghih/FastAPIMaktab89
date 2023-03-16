6d973d3ace396220aedca831aab6295ad7743878
Sayyed Mohammad Razavi <smrazavi1991@hotmail.com>
2023-03-16 17:08:15 +0330

    Add a validator to validate password field value and password confirmation value

--- app/Auth/Schema.py
+++ app/Auth/Schema.py
@@ -1,5 +1,4 @@
-from pydantic import BaseModel, Field
-
+from pydantic import BaseModel, Field, validator
 
 
 class LoginRequest(BaseModel):
@@ -12,4 +11,13 @@ class RegisterNewUser(BaseModel):
     username: str
     email: str = Field(regex=r"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$")
     password: str = Field(regex=r"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$")
+    password_confirmation: str
+
+    @validator('password_confirmation')
+    @classmethod
+    def check_password_confirmation(cls, value, values):
+        if values["password"] != value:
+            raise ValueError('Password Confirmation must match password')
+
+        return value
 

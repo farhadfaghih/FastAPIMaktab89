d24ce6dafa1551c67668e56b0022837cb4e79ff9
Sayyed Mohammad Razavi <smrazavi1991@hotmail.com>
2023-03-27 01:13:03 +0330

    Final inspection and some cleaning

--- app/Core/database.py
+++ app/Core/database.py
@@ -3,15 +3,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import hashlib
 
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./Core/sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://postgres@/blog"
-#pip install psycopg2-binary
-#run psql with "sudo --login --user=postgres psql"
-#create a database called blog
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./Core/sql_app.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mamali1991@/blog"
+# pip install psycopg2-binary
+# run psql with "sudo --login --user=postgres psql"
+# create a database called blog
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, 
-    connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL,
+    # connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
